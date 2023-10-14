2f7ef0660d5f14191379f867f2c47378db5c04a2
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-10-14 09:11:19 +0545

    Minor change

--- person.rb
+++ person.rb
@@ -2,39 +2,36 @@ require_relative 'nameable'
 require_relative 'rental'
 
 class Person < Nameable
-  def initialize(age, parent_permission, name = 'Unknown')
-    super()
+  attr_accessor :name, :age, :rentals, :parent_permission
+  attr_reader :id
 
-    @id = Random.rand(1..1000)
+  def initialize(name = 'Unknown', age = 0, parent_permission: true, nameable: nil)
+    super()
+    @id = Random.rand(1...100)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @nameable = nameable
     @rentals = []
-  end
-
-  # getters
-  attr_reader :id, :parent_permission, :rentals
-  attr_accessor :name, :age
-
-  def can_use_services?
-    return true if of_age?
-
-    false
-  end
-
-  def correct_name
-    @name
-  end
-
-  def add_rental(book, date)
-    Rental.new(date, book, self)
   end
 
   private
 
   def of_age?
-    return true if age >= 18
+    @age >= 18
+  end
 
-    false
+  public
+
+  def can_use_service?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    name
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, book, self)
   end
 end
