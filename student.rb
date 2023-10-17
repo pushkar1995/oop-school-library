46aca27635783d49f14bee6db40895b50415e1ba
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-10-17 17:03:02 +0545

    Minor fix

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Student < Person
   attr_reader :classroom
-  
+
   def initialize(name = 'Unknown', age = 0, parent_permission: true, classroom: nil)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
@@ -16,5 +16,4 @@ class Student < Person
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
-
 end
